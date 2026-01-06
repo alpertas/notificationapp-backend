ef53fc514790a6d2fd61a521fc52e5296c88a7a0
alpertas <alpertas.cpp@gmail.com>
2026-01-06 18:59:07 +0300

    feat: integrate auth and notification modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirebaseModule } from './firebase/firebase.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    FirebaseModule,
+    PrismaModule,
+    AuthModule,
+    NotificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
